a0df72fa8974a9fe5ac3ea73212c932aca46542b
AdamBuckly <adambuckley@live.com>
2016-01-28 10:07:36 +0000

    Updated computer to win method (it is still a work in progress).

--- src/tictactoelab02/TicTacToeLab02.java
+++ src/tictactoelab02/TicTacToeLab02.java
@@ -27,7 +27,8 @@ import javax.swing.border.LineBorder;
  * 
  * @author Adam Buckley (Student I.D: 20062910).
  * @version 1.
- * @date 15/01/2016.
+ * @date Started writing: 12/01/2016.
+ * @date Last day edited: 
  */
 
 public class TicTacToeLab02{
@@ -39,124 +40,124 @@ public class TicTacToeLab02{
 	final static int X_SHAPE = 1;
 	final static int O_SHAPE = -1;
 	final static Random random = new Random();
-	
+
 	//array of booleans, the 5 usable numbers for the computer.
 	boolean[] booleanArray = new boolean[5];
 
 	public static void main(String[] args) 
 	{
-			// Allocate identifiers to represent game state
-			// Using an array of int so that summing along a row, a column or a
-			// diagonal is a easy test for a win
-			//Frame frame = new JFrame("Game Of Tic-Tac-Toe");
+		// Allocate identifiers to represent game state
+		// Using an array of int so that summing along a row, a column or a
+		// diagonal is a easy test for a win
+		//Frame frame = new JFrame("Game Of Tic-Tac-Toe");
 
-			int[][] board = new int[3][3];
+		int[][] board = new int[3][3];
 
-			//array of booleans, the 5 usable numbers for the computer.
-			boolean[] booleanArray = new boolean[5];
+		//array of booleans, the 5 usable numbers for the computer.
+		boolean[] booleanArray = new boolean[5];
 
-			int row = 0;
-			int col = 0;
-			int move = 0;
-			boolean boardFull;
-			boolean playerWon;
+		int row = 0;
+		int col = 0;
+		int move = 0;
+		boolean boardFull;
+		boolean playerWon;
 
-			// Setup graphics and draw empty board
-			StdDraw.setPenRadius(0.04);							// draw thicker lines
-			StdDraw.line(0, 0.33, 1, 0.33);
-			StdDraw.line(0, 0.66, 1, 0.66);
-			StdDraw.line(0.33, 0, 0.33, 1.0);
-			StdDraw.line(0.66, 0, 0.66, 1.0);
+		// Setup graphics and draw empty board
+		StdDraw.setPenRadius(0.04);							// draw thicker lines
+		StdDraw.line(0, 0.33, 1, 0.33);
+		StdDraw.line(0, 0.66, 1, 0.66);
+		StdDraw.line(0.33, 0, 0.33, 1.0);
+		StdDraw.line(0.66, 0, 0.66, 1.0);
 
-			StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
+		StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
 
-			do {
-				if(move % 2 == 1)
-				{
-					System.out.println("\tHuman move ...");
-
-
-					displayUserMovePrompt();
-
-					int theNumber = choicePopUp(board);
-
-					// use mouse position to get slot
-					boolean mousePressed = false;
-					do {
-						if (StdDraw.mousePressed()) {
-
-							col = (int) (StdDraw.mouseX() * 3);	
-
-							row = (int) (StdDraw.mouseY() * 3);
-
-							mousePressed = true;
-						}
-					}while(!mousePressed || board[row][col] != EMPTY);
-
-					//int theNumber = 5;
-
-					board[row][col] = theNumber;   // valid move (empty slot)
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
-					//StdDraw.clear(0.5, 0.1, StdDraw.LIGHT_GRAY);
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
-				}
-				else
-				{
-					System.out.println("\tComputer move ...");
-
-					displayCompMovePrompt();
-
-					int[] pos = getEmptySquare(board);
-					row = pos[0];
-					col = pos[1];
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
-					board[row][col] = pickingNumberComp(booleanArray, board);   // valid move (empty slot)
-					//keep a time of 650ms for the computer to make a move
-					//(helps the player experience feel more authentic).
-					try {
-						Thread.sleep(650);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
-					StdDraw.text(0.5, 0.1, "           ");
-					StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
-				}
-
-				// Update screen to reflect board change
-				// move is the move number, col is the current move's column, row 
-				//is the current move's row
-				double x = col * .33 + 0.15;
-				double y = row * .33 + 0.15;
-				//Directly below: board reflecting the change: if the move int value is an even
-				//number then the user's X value position choice is reflected but it move is an
-				//odd number the computer's (random) position choice is reflected on this update.
-				StdDraw.text(x, y, String.valueOf(board[row][col]));
-				move++;
-				System.out.println(move);
-				boardFull = isBoardFull(board);
-				playerWon = hasSomebodyWon(board);
-			}while (!boardFull && !playerWon);
-
-			if (playerWon == true)
+		do {
+			if(move % 2 == 1)
 			{
-				if (move % 2 == 1)
-				{
-					JOptionPane.showMessageDialog(null, "Game Over: You Lost");
-				}
-				else if (move % 2 == 0)
-				{
-					JOptionPane.showMessageDialog(null, "Game Over: You Won");
-				}
+				System.out.println("\tHuman move ...");
+
+
+				displayUserMovePrompt();
+
+				int theNumber = choicePopUp(board);
+
+				// use mouse position to get slot
+				boolean mousePressed = false;
+				do {
+					if (StdDraw.mousePressed()) {
+
+						col = (int) (StdDraw.mouseX() * 3);	
+
+						row = (int) (StdDraw.mouseY() * 3);
+
+						mousePressed = true;
+					}
+				}while(!mousePressed || board[row][col] != EMPTY);
+
+				//int theNumber = 5;
+
+				board[row][col] = theNumber;   // valid move (empty slot)
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
+				//StdDraw.clear(0.5, 0.1, StdDraw.LIGHT_GRAY);
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
 			}
 			else
 			{
-				JOptionPane.showMessageDialog(null, "It's a Draw");
+				System.out.println("\tComputer move ...");
+
+				displayCompMovePrompt();
+
+				int[] pos = getEmptySquare(board);
+				row = pos[0];
+				col = pos[1];
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
+				board[row][col] = pickingNumberComp(booleanArray, board);   // valid move (empty slot)
+				//keep a time of 650ms for the computer to make a move
+				//(helps the player experience feel more authentic).
+				try {
+					Thread.sleep(650);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 20)); // Font SIZE!
+				StdDraw.text(0.5, 0.1, "           ");
+				StdDraw.setFont(new Font("SansSerif", Font.PLAIN, 64)); // Font SIZE!
 			}
-			System.out.println("The Game is over");
+
+			// Update screen to reflect board change
+			// move is the move number, col is the current move's column, row 
+			//is the current move's row
+			double x = col * .33 + 0.15;
+			double y = row * .33 + 0.15;
+			//Directly below: board reflecting the change: if the move int value is an even
+			//number then the user's X value position choice is reflected but it move is an
+			//odd number the computer's (random) position choice is reflected on this update.
+			StdDraw.text(x, y, String.valueOf(board[row][col]));
+			move++;
+			System.out.println(move);
+			boardFull = isBoardFull(board);
+			playerWon = hasSomebodyWon(board);
+		}while (!boardFull && !playerWon);
+
+		if (playerWon == true)
+		{
+			if (move % 2 == 1)
+			{
+				JOptionPane.showMessageDialog(null, "Game Over: You Lost");
+			}
+			else if (move % 2 == 0)
+			{
+				JOptionPane.showMessageDialog(null, "Game Over: You Won");
+			}
 		}
-	
+		else
+		{
+			JOptionPane.showMessageDialog(null, "It's a Draw");
+		}
+		System.out.println("The Game is over");
+	}
+
 	/**
 	 * This method allows a pop up to appear right before the very first game
 	 * is about to be played. This pop-up gives a user an option to play the game
@@ -318,28 +319,40 @@ public class TicTacToeLab02{
 		return false;
 	}
 
+	/**
+	 * This method is the intelligent way the computer automatically
+	 * "grabs" a win when a potential win is in place. 
+	 * (board is checked after every round in the main method).
+	 * @param booleanArray
+	 * @param board
+	 * @return
+	 */
 	public static int[] computerToWin(boolean booleanArray[], int board[][])
 	{
 		{
+			// Below: because there is 5 options for he computer.
+			// (obviously one value in this array will already be true after the first number
+			// is placed on the board on the initial turn.
 			for (int i = 0; i < 5; i++)
 			{
-				// Below: check: if the position in the array (from 0 to 4), if the value in the boolean array
-				// is true then continue within:
+				// Below: check: if the position in the array (from 0 to 4). If the particular value in
+				// the boolean array is true then continue within:
 				if (booleanArray[i])
 				{
-					// Get the actual number
+					// Get the actual number itself;
 					int number = (i*2) + 1;
 
 					for (int row = 0; row < 3; row++)
 					{
 						// Below: check to see if exactly 1 of the squares is empty and two are occupied
-						if (!(board[row][0] != 0 || board[row][1] != 0 || board[row][2] != 0))
+						if (!(board[row][0] != 0 || board[row][1] != 0 || board[row][2] != 0)
+								&& !(board[row][0] == 0 && board[row][1] == 0 && board[row][2] == 0))
 						{
 							int sum = board[row][0] + board[row][1] + board[row][2];
 
 							for (int col = 0; col < 3; col++)
 							{
-								// Below: if the two cells in question and the candidate (candidate = number 
+								// Below: if the two filled cells in question and the candidate (candidate = number 
 								// to potentially maybe be put in) equal to 15 then go within:
 								if (sum + number == 15)
 								{
@@ -351,6 +364,9 @@ public class TicTacToeLab02{
 						}
 					}
 				}
+			}
+			
+			{
 
 				for (int j = 0; j < 5; j++)
 				{
@@ -364,7 +380,8 @@ public class TicTacToeLab02{
 						for (int col = 0; col < 3; col++)
 						{
 							// Below: check to see if exactly 1 of the squares is empty and two are occupied
-							if (!(board[0][col] != 0 || board[1][col] != 0 || board[2][col] != 0))
+							if (!(board[0][col] != 0 || board[1][col] != 0 || board[2][col] != 0)
+									&& !(board[0][col] == 0 && board[1][col] == 0 && board[2][col] == 0))
 							{
 								int sum = board[0][col] + board[1][col] + board[2][col];
 
@@ -383,6 +400,86 @@ public class TicTacToeLab02{
 						}
 					}
 				}
+			}
+				
+			{
+				//now diagonals must be done:
+				
+				// Below: because there is 5 options for he computer.
+				// (obviously one value in this array will already be true after the first number
+				// is placed on the board on the initial turn.
+				for (int i = 0; i < 5; i++)
+				{
+					// Below: check: if the position in the array (from 0 to 4). If the particular value in
+					// the boolean array is true then continue within:
+					if (booleanArray[i])
+					{
+						// Get the actual number itself;
+						int number = (i*2) + 1;
+
+				int diagonalSum = board[0][0] + board[1][1] + board[2][2];
+				int diagonalSum2 = board[0][2] + board[1][1] + board[2][0];
+
+				if (diagonalSum + number == 15)
+				{
+					if (!(board[0][0] != 0 || board[1][1] != 0 || board[2][2] != 0)
+							&& !(board[0][0] == 0 && board[1][1] == 0 && board[2][2] == 0))
+					{
+						for (int k = 0; k < 3; k++)
+							if (board[k][k] == EMPTY)
+							{
+								int[] itelligentChosenEmptySquare = {k, k};
+								return itelligentChosenEmptySquare;
+							}
+					}
+
+					if (diagonalSum2 + number == 15)
+					{
+						if (!(board[0][2] != 0 || board[1][1] != 0 || board[2][0] != 0)
+								&& !(board[0][2] == 0 && board[1][1] == 0 && board[2][0] == 0))
+						{
+							for (int row = 0; row < 3; row++)
+								for (int col = 0; col < 3; col++)
+								{
+									// below while: only board postions [0][2], [1][1] and [2][0] will
+									// enter below's while loop:
+									while (row+col == 2)
+									{
+										// One of them has to be empty (see above closest above if statement).
+										if (board[row][col] == EMPTY)
+										{
+											// now return that empty col position
+											int[] itelligentChosenEmptySquare = {row, col};
+											return itelligentChosenEmptySquare;
+										}
+									}
+								}
+						}
+					}
+				}
+				}
+			}
+				return null;
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 
 
 
@@ -394,9 +491,9 @@ public class TicTacToeLab02{
 			}
 		}
 
-		
-		
-		
+
+
+
 		{
 			for (int row = 0; row < 3; row++)
 			{
