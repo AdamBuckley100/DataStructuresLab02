c7b1aa494803ab03ce63d263ccab823918e88629
AdamBuckly <adambuckley@live.com>
2016-01-30 13:24:30 +0000

    Indentation change.

--- src/tictactoelab02/TicTacToeLab02.java
+++ src/tictactoelab02/TicTacToeLab02.java
@@ -352,63 +352,63 @@ public class TicTacToeLab02{
 								{
 									col = 2;
 								}
-							// Return that specific row,col combination cause this will let the computer win
-							int[] itelligentChosenEmptySquare = {row, col, number};
-							return itelligentChosenEmptySquare;
-								}
-								}
+								// Return that specific row,col combination cause this will let the computer win
+								int[] itelligentChosenEmptySquare = {row, col, number};
+								return itelligentChosenEmptySquare;
 							}
-
 						}
 					}
+
 				}
+			}
+		}
 
-			for (int j = 0; j < 5; j++)
+		for (int j = 0; j < 5; j++)
+		{
+			// Below: check: if the position in the array (from 0 to 4), if the value in the boolean array
+			// is true then continue within:
+			if (!booleanArray[j])
 			{
-				// Below: check: if the position in the array (from 0 to 4), if the value in the boolean array
-				// is true then continue within:
-				if (!booleanArray[j])
+				// Get the actual number
+				int number = (j*2) + 1;
+
+				for (int col = 0; col < 3; col++)
 				{
-					// Get the actual number
-					int number = (j*2) + 1;
-
-					for (int col = 0; col < 3; col++)
+					// Below: check to see if exactly 1 of the squares is empty and two are occupied
+					if ((board[0][col] == 0 && board[1][col] != 0 && board[2][col] !=0)
+							|| (board[0][col] != 0 && board[1][col] == 0 && board[2][col] !=0)
+							|| (board[0][col] != 0 && board[1][col] != 0 && board[2][col] ==0))
 					{
-						// Below: check to see if exactly 1 of the squares is empty and two are occupied
-						if ((board[0][col] == 0 && board[1][col] != 0 && board[2][col] !=0)
-								|| (board[0][col] != 0 && board[1][col] == 0 && board[2][col] !=0)
-								|| (board[0][col] != 0 && board[1][col] != 0 && board[2][col] ==0))
-						{
-							int sum = board[0][col] + board[1][col] + board[2][col];
+						int sum = board[0][col] + board[1][col] + board[2][col];
 
-							
+
+						{
+							// Below: if the two cells in question and the candidate (candidate = number 
+							// to potentially maybe be put in) equal to 15 then go within:
+							if (sum + number == 15)
 							{
-								// Below: if the two cells in question and the candidate (candidate = number 
-								// to potentially maybe be put in) equal to 15 then go within:
-								if (sum + number == 15)
+								int row = 0;
+								if (board[0][col] == 0)
 								{
-									int row = 0;
-									if (board[0][col] == 0)
-									{
-										row = 0;
-									}
-									else if (board[1][col] == 0)
-									{
-										row = 1;
-									}
-									else if (board[2][col] == 0)
-									{
-										row = 2;
-									}
-									// Return that specific row,col combination cause this will let the computer win
-									int[] itelligentChosenEmptySquare = {row, col, number};
-									return itelligentChosenEmptySquare;
+									row = 0;
 								}
+								else if (board[1][col] == 0)
+								{
+									row = 1;
+								}
+								else if (board[2][col] == 0)
+								{
+									row = 2;
+								}
+								// Return that specific row,col combination cause this will let the computer win
+								int[] itelligentChosenEmptySquare = {row, col, number};
+								return itelligentChosenEmptySquare;
 							}
 						}
 					}
 				}
 			}
+		}
 
 		{
 			//now diagonals must be done:
@@ -473,21 +473,21 @@ public class TicTacToeLab02{
 		}
 	}
 
-public static int pickingNumberCompRandom(boolean[] booleanArray, int[][] board)
-{
-	int number;
-
-	do 
+	public static int pickingNumberCompRandom(boolean[] booleanArray, int[][] board)
 	{
-		number = random.nextInt(9) + 1;
+		int number;
+
+		do 
+		{
+			number = random.nextInt(9) + 1;
+		}
+		while (number % 2 == 0 || hasANumberBeenUsed(board, number));
+
+		booleanArray[(number-1)/(2)] = false;
+		return number;
 	}
-	while (number % 2 == 0 || hasANumberBeenUsed(board, number));
 
-	booleanArray[(number-1)/(2)] = false;
-	return number;
-}
-
-/*public static boolean pickingNumberCompNonRandomCheck(int number, int[][] board)
+	/*public static boolean pickingNumberCompNonRandomCheck(int number, int[][] board)
 	{
 		if (!hasANumberBeenUsed(board, number))
 		{
@@ -496,67 +496,32 @@ public static int pickingNumberCompRandom(boolean[] booleanArray, int[][] board)
 		return false; // false = don't use the no.
 	}*/
 
-public static int[] computerToBlock(boolean humanBooleanArray[], int board[][])
-{
+	public static int[] computerToBlock(boolean humanBooleanArray[], int board[][])
 	{
-		// Below: because there is 5 options for the computer.
-		// (Obviously one value in this array will already be true (used) after the first number
-		// is placed on the board on the initial turn.
-		for (int i = 0; i < 5; i++)
 		{
-			// Below: check: if the position in the array (from 0 to 4). If the particular value in
-			// the boolean array is false (unused) then continue within:
-			if (!humanBooleanArray[i])
+			// Below: because there is 5 options for the computer.
+			// (Obviously one value in this array will already be true (used) after the first number
+			// is placed on the board on the initial turn.
+			for (int i = 0; i < 5; i++)
 			{
-				// Get the actual number itself;
-				int number = (i*2) + 1;
-
-				for (int row = 0; row < 3; row++)
+				// Below: check: if the position in the array (from 0 to 4). If the particular value in
+				// the boolean array is false (unused) then continue within:
+				if (!humanBooleanArray[i])
 				{
-					// Below: check to see if exactly 1 of the squares is empty and two are occupied
-					if ((board[row][0] == 0 || board[row][1] == 0 || board[row][2] == 0)
-							&& !(board[row][0] == 0 && board[row][1] == 0 && board[row][2] == 0))
-					{
-						int sum = board[row][0] + board[row][1] + board[row][2];
+					// Get the actual number itself;
+					int number = (i*2) + 1;
 
-						for (int col = 0; col < 3; col++)
-						{
-							// Below: if the two filled cells in question and the candidate (candidate = number 
-							// to potentially maybe be put in) equal to 15 then go within:
-							if (sum + number == 15)
-							{
-								// Return that specific row,col combination cause this will let the computer win
-								int[] itelligentChosenEmptySquare = {row, col, number};
-								return itelligentChosenEmptySquare;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		{
-
-			for (int j = 0; j < 5; j++)
-			{
-				// Below: check: if the position in the array (from 0 to 4), if the value in the boolean array
-				// is false (unused) then continue within:
-				if (!humanBooleanArray[j])
-				{
-					// Get the actual number
-					int number = (j*2) + 1;
-
-					for (int col = 0; col < 3; col++)
+					for (int row = 0; row < 3; row++)
 					{
 						// Below: check to see if exactly 1 of the squares is empty and two are occupied
-						if ((board[0][col] == 0 || board[1][col] == 0 || board[2][col] == 0)
-								&& !(board[0][col] == 0 && board[1][col] == 0 && board[2][col] == 0))
+						if ((board[row][0] == 0 || board[row][1] == 0 || board[row][2] == 0)
+								&& !(board[row][0] == 0 && board[row][1] == 0 && board[row][2] == 0))
 						{
-							int sum = board[0][col] + board[1][col] + board[2][col];
+							int sum = board[row][0] + board[row][1] + board[row][2];
 
-							for (int row = 0; row < 3; row++)
+							for (int col = 0; col < 3; col++)
 							{
-								// Below: if the two cells in question and the candidate (candidate = number 
+								// Below: if the two filled cells in question and the candidate (candidate = number 
 								// to potentially maybe be put in) equal to 15 then go within:
 								if (sum + number == 15)
 								{
@@ -569,67 +534,102 @@ public static int[] computerToBlock(boolean humanBooleanArray[], int board[][])
 					}
 				}
 			}
-		}
 
-		{
-			//now diagonals must be done:
-
-			// Below: because there is 5 options for he computer.
-			// (obviously one value in this array will already be true after the first number
-			// is placed on the board on the initial turn.
-			for (int i = 0; i < 5; i++)
 			{
-				// Below: check: if the position in the array (from 0 to 4). If the particular value in
-				// the boolean array is false (unused) then continue within:
-				if (!humanBooleanArray[i])
+
+				for (int j = 0; j < 5; j++)
 				{
-					// Get the actual number itself;
-					int number = (i*2) + 1;
-
-					int diagonalSum = board[0][0] + board[1][1] + board[2][2];
-					int diagonalSum2 = board[0][2] + board[1][1] + board[2][0];
-
-					if (diagonalSum + number == 15)
+					// Below: check: if the position in the array (from 0 to 4), if the value in the boolean array
+					// is false (unused) then continue within:
+					if (!humanBooleanArray[j])
 					{
-						if (!(board[0][0] != 0 || board[1][1] != 0 || board[2][2] != 0)
-								&& !(board[0][0] == 0 && board[1][1] == 0 && board[2][2] == 0))
-						{
-							for (int k = 0; k < 3; k++)
-								if (board[k][k] == EMPTY)
-								{
-									int[] itelligentChosenEmptySquare = {k, k, number};
-									return itelligentChosenEmptySquare;
-								}
-						}
+						// Get the actual number
+						int number = (j*2) + 1;
 
-						if (diagonalSum2 + number == 15)
+						for (int col = 0; col < 3; col++)
 						{
-							if (!(board[0][2] != 0 || board[1][1] != 0 || board[2][0] != 0)
-									&& !(board[0][2] == 0 && board[1][1] == 0 && board[2][0] == 0))
+							// Below: check to see if exactly 1 of the squares is empty and two are occupied
+							if ((board[0][col] == 0 || board[1][col] == 0 || board[2][col] == 0)
+									&& !(board[0][col] == 0 && board[1][col] == 0 && board[2][col] == 0))
 							{
+								int sum = board[0][col] + board[1][col] + board[2][col];
+
 								for (int row = 0; row < 3; row++)
-									for (int col = 0; col < 3; col++)
+								{
+									// Below: if the two cells in question and the candidate (candidate = number 
+									// to potentially maybe be put in) equal to 15 then go within:
+									if (sum + number == 15)
 									{
-										// below while: only board positions [0][2], [1][1] and [2][0] will
-										// enter below's while loop:
-										while ((row+col) + number == 15)
-										{
-											// One of them has to be empty (see above closest above if statement).
-											if (board[row][col] == EMPTY)
-											{
-												// now return that empty col position
-												int[] itelligentChosenEmptySquare = {row, col, number};
-												return itelligentChosenEmptySquare;
-											}
-										}
+										// Return that specific row,col combination cause this will let the computer win
+										int[] itelligentChosenEmptySquare = {row, col, number};
+										return itelligentChosenEmptySquare;
 									}
+								}
 							}
 						}
 					}
 				}
 			}
-			return null;
+
+			{
+				//now diagonals must be done:
+
+				// Below: because there is 5 options for he computer.
+				// (obviously one value in this array will already be true after the first number
+				// is placed on the board on the initial turn.
+				for (int i = 0; i < 5; i++)
+				{
+					// Below: check: if the position in the array (from 0 to 4). If the particular value in
+					// the boolean array is false (unused) then continue within:
+					if (!humanBooleanArray[i])
+					{
+						// Get the actual number itself;
+						int number = (i*2) + 1;
+
+						int diagonalSum = board[0][0] + board[1][1] + board[2][2];
+						int diagonalSum2 = board[0][2] + board[1][1] + board[2][0];
+
+						if (diagonalSum + number == 15)
+						{
+							if (!(board[0][0] != 0 || board[1][1] != 0 || board[2][2] != 0)
+									&& !(board[0][0] == 0 && board[1][1] == 0 && board[2][2] == 0))
+							{
+								for (int k = 0; k < 3; k++)
+									if (board[k][k] == EMPTY)
+									{
+										int[] itelligentChosenEmptySquare = {k, k, number};
+										return itelligentChosenEmptySquare;
+									}
+							}
+
+							if (diagonalSum2 + number == 15)
+							{
+								if (!(board[0][2] != 0 || board[1][1] != 0 || board[2][0] != 0)
+										&& !(board[0][2] == 0 && board[1][1] == 0 && board[2][0] == 0))
+								{
+									for (int row = 0; row < 3; row++)
+										for (int col = 0; col < 3; col++)
+										{
+											// below while: only board positions [0][2], [1][1] and [2][0] will
+											// enter below's while loop:
+											while ((row+col) + number == 15)
+											{
+												// One of them has to be empty (see above closest above if statement).
+												if (board[row][col] == EMPTY)
+												{
+													// now return that empty col position
+													int[] itelligentChosenEmptySquare = {row, col, number};
+													return itelligentChosenEmptySquare;
+												}
+											}
+										}
+								}
+							}
+						}
+					}
+				}
+				return null;
+			}
 		}
 	}
-}
 }
